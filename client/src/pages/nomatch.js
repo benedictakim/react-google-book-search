ebd4913aa14298da9b7f87ce395c5f05c9aa04a3
Benn <61118590+benedictakim@users.noreply.github.com>
2020-10-20 23:01:06 -0400

    front end react code from https://github.com/Victoire44 in /client/src

---
+++ client/src/pages/nomatch.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "../components/Header";
+
+function NoMatch() {
+  return (
+    <div>
+          <Header>
+            <h1>404 Page Not Found</h1>
+            <h1>
+              <span role="img" aria-label="shrug emoji">
+              ¯\_(ツ)_/¯ 
+              </span>
+            </h1>
+          </Header>
+    </div>
+  );
+}
+
+export default NoMatch;
+© 2020 GitHub, Inc.
